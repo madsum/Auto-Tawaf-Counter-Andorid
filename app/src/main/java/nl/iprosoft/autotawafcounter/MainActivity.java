0eed657e5bf395cb17aabbdd8652a97d3b475000
iprosoft-nl <masum.islam2@gmail.com>
2025-04-24 13:23:10 +0200

    Ask location permission

--- app/src/main/java/nl/iprosoft/autotawafcounter/MainActivity.java
+++ app/src/main/java/nl/iprosoft/autotawafcounter/MainActivity.java
@@ -1,14 +1,19 @@
 package nl.iprosoft.autotawafcounter;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.Manifest;
 
 import nl.iprosoft.autotawafcounter.databinding.ActivityMainBinding;
 
@@ -19,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Runtime permission check
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -32,6 +46,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Permission granted
+            Toast.makeText(this, "Location permission granted ✅", Toast.LENGTH_SHORT).show();
+        } else {
+            // Permission denied
+            Toast.makeText(this, "Location permission denied ❌", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
