9a73862f2eb94a9153b5645eb1fa1db525a2ac79
iprosoft-nl <masum.islam2@gmail.com>
2025-05-01 11:20:44 +0200

    Implement Main Activity and HomeFragment

--- app/src/main/java/nl/iprosoft/autotawafcounter/ui/home/HomeFragment.java
+++ app/src/main/java/nl/iprosoft/autotawafcounter/ui/home/HomeFragment.java
@@ -1,32 +1,83 @@
 package nl.iprosoft.autotawafcounter.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import nl.iprosoft.autotawafcounter.R;
+import nl.iprosoft.autotawafcounter.activity.TawafActivity;
 import nl.iprosoft.autotawafcounter.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
-
+    private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
+         this.homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Observe navigation state changes
+        homeViewModel.getNavigationState().observe(getViewLifecycleOwner(), state -> {
+            switch (state) {
+                case HomeViewModel.STATE_INITIAL:
+                    showAlignmentDialog();
+                    break;
+                case HomeViewModel.STATE_NOT_ALIGNED:
+                    showNotAlignedDialog();
+                    break;
+                case HomeViewModel.STATE_START_TAWAF:
+                    startTawafActivity();
+                    break;
+            }
+        });
+
         return root;
+    }
+
+private void showAlignmentDialog() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+    builder.setTitle(R.string.dialog_alignment_title)
+            .setMessage(R.string.dialog_alignment_message)
+            .setCancelable(false)
+            .setPositiveButton(R.string.button_yes_start_tawaf, (dialog, which) -> {
+                homeViewModel.onYesStartTawafClicked();
+            })
+            .setNegativeButton(R.string.button_not_yet, (dialog, which) -> {
+                homeViewModel.onNotYetClicked();
+            });
+
+    AlertDialog dialog = builder.create();
+    dialog.show();
+}
+
+    private void showNotAlignedDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle(R.string.dialog_not_aligned_title)
+                .setMessage(R.string.dialog_not_aligned_message)
+                .setCancelable(false)
+                .setPositiveButton(R.string.button_ok, (dialog, which) -> {
+                    homeViewModel.onNotAlignedOkClicked();
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    private void startTawafActivity() {
+        Intent intent = new Intent(requireContext(), TawafActivity.class);
+        startActivity(intent);
     }
 
     @Override
--- app/src/main/java/nl/iprosoft/autotawafcounter/ui/home/HomeViewModel.java
+++ app/src/main/java/nl/iprosoft/autotawafcounter/ui/home/HomeViewModel.java
@@ -6,14 +6,40 @@ import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    // Define constants for navigation state
+    public static final int STATE_INITIAL = 0;
+    public static final int STATE_NOT_ALIGNED = 1;
+    public static final int STATE_START_TAWAF = 2;
+//    private final MutableLiveData<String> mText;
+
+    // LiveData to track the dialog state
+    private final MutableLiveData<Integer> navigationState = new MutableLiveData<>();
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        // Initialize with the alignment question state
+        navigationState.setValue(STATE_INITIAL);
     }
 
-    public LiveData<String> getText() {
+/*    public LiveData<String> getText() {
         return mText;
+    }*/
+
+    // Method to get the current navigation state
+    public LiveData<Integer> getNavigationState() {
+        return navigationState;
+    }
+
+    // Methods to update state based on user actions
+    public void onYesStartTawafClicked() {
+        navigationState.setValue(STATE_START_TAWAF);
+    }
+
+    public void onNotYetClicked() {
+        navigationState.setValue(STATE_NOT_ALIGNED);
+    }
+
+    public void onNotAlignedOkClicked() {
+        // Go back to the initial question
+        navigationState.setValue(STATE_INITIAL);
     }
 }
